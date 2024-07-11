1ff6924411cc2f37ee0387fe8e0887492d913a20
Steven barnett <steve@sabarnett.co.uk>
2024-07-11 13:28:07 +0100

    Update file headers

--- windowReferenceApp/HostingWindowFinder.swift
+++ windowReferenceApp/HostingWindowFinder.swift
@@ -1,8 +1,11 @@
 //
-// File: HostingWindowFinder.swift
-// Package: windowReferenceApp
-// Created by: Steven Barnett on 02/07/2024
-// 
+// -----------------------------------------
+// Original project: windowReferenceApp
+// Original package: windowReferenceApp
+// Created on: 11/07/2024 by: Steven Barnett
+// Web: http://www.sabarnett.co.uk
+// GitHub: https://www.github.com/sabarnett
+// -----------------------------------------
 // Copyright © 2024 Steven Barnett. All rights reserved.
 //
 
